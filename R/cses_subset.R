1744903ac8bd3288e966826a78a852b2283a9208
Joshua de la Bruere <j.l.delabruere@gmail.com>
2020-11-10 14:37:57 -0600

    add dataset description

--- R/cses_subset.R
+++ R/cses_subset.R
@@ -1,6 +1,13 @@
 #' A Subset of data from the CSES
 #'
-#' @description A subset of data from Modules 1,2,3,4 and the advanced release of Module 5. Columns selected were common between multiple
+#' @description A subset of data from Modules 1,2,3,4 and the advanced release of Module 5. Columns selected were common between multiple modules.
+#' Data is left in the base encoded form from the CSES. Data included is polity information, election year, study timing, demographic information like
+#' gender, education, maritial status, employment status, household income level, as well as information about satisfaction with democracy, self rating
+#' on a left right scale. In addition columns relating to parties A-F are included as well.
+#'
+#'
+#' @return a dataset with 317551 rows across 88 columns
+#'
 #'
 #' @export
 'cses_subset'

95d58d86255b80d8be5f1758485d580c7766a89d
Joshua de la Bruere <j.l.delabruere@gmail.com>
2020-11-10 14:14:17 -0600

    Add internal data 📈

---
+++ R/cses_subset.R
@@ -0,0 +1,6 @@
+#' A Subset of data from the CSES
+#'
+#' @description A subset of data from Modules 1,2,3,4 and the advanced release of Module 5. Columns selected were common between multiple
+#'
+#' @export
+'cses_subset'
